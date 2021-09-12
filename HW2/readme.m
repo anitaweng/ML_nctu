9a74ad606d43eb6edce1ac8ba6e24810b8a867d0
Anita Weng <42642215+anitaweng@users.noreply.github.com>
2021-09-12 20:21:38 +0800

    Update readme.m

--- HW2/readme.m
+++ HW2/readme.m
@@ -1,13 +1,13 @@
-%執行順序
+%execution order
 %Q1->Q1_2->Q1_3
 %Q2->Q2_PCA
 %Q2_2_pca->Q2_2
 %Q3
 
-%須放在和dataset同一路徑
-%須有一result_1資料夾，下層有1,2,3
+%The files needs to be in the same directory as dataset.
+%One should create a 'result_1' folder with subfolder 1,2,3, for example:
 %result_1/1
 %result_1/2
 %result_1/3
 
-%Q1_2須和fill_between.m及sigmoid.m同一層
+%Q1_2 should be in the same folder as fill_between.m and sigmoid.m.
